eabb118f64ad126e705cfb6221e3839ee9b82ecc
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-01-19 23:52:20 +0530

    doe

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let sign_in_display = document.querySelector(".signin")
 let sign_in_hover = document.getElementById("open_sign_in")
 
@@ -11,8 +12,147 @@ sign_in_display.addEventListener("mouseleave",()=>{
 
 let login = document.querySelector("#sign button")
 let reg = document.getElementById("reg")
-console.log(login)
+
 
 login.addEventListener("click",()=>{
     reg.style.visibility = "visible"
 })
+
+let register_button = document.querySelectorAll("#reg_button")
+
+
+let reg2 = document.getElementById("reg2")
+    register_button[0].addEventListener("click",()=>{
+        reg.style.visibility = "hidden"
+        reg2.style.visibility = "visible"
+    })
+    register_button[1].addEventListener("click",()=>{
+        reg.style.visibility = "hidden"
+        reg2.style.visibility = "visible"
+    })
+
+let login_button = document.querySelectorAll("#login_button")
+   
+    login_button[0].addEventListener("click",()=>{
+        reg2.style.visibility = "hidden"
+        reg.style.visibility = "visible"
+    })
+
+    login_button[1].addEventListener("click",()=>{
+        reg2.style.visibility = "hidden"
+        reg.style.visibility = "visible"
+    })
+
+    login_button[2].addEventListener("click",()=>{
+        reg2.style.visibility = "hidden"
+        reg.style.visibility = "visible"
+        verify.style.visibility = "hidden"
+    })
+
+    let registration_done = document.getElementById("registration_done")
+    let sp = ".@"
+    let login_form = document.getElementById("login_form")
+    let verify = document.getElementById("verify")
+        login_form.addEventListener("submit",(e)=>{
+            e.preventDefault()
+            if (login_form.mobile.value.length  >= 10 || login_form.mobile.value.includes(".")===true && login_form.mobile.value.includes("@")===true ){
+                reg2.style.visibility = "hidden"
+                reg.style.visibility = "hidden"
+                verify.style.visibility = "visible"
+                login_task(login_form)
+                
+            }else {
+                alert(" Enter Mobile or email")
+            }
+
+
+        })
+
+        let close_form = document.querySelectorAll("#close")
+
+        for (let i = 0 ; i < close_form.length ;i++){
+            close_form[i].addEventListener("click",()=>{
+                reg2.style.visibility = "hidden"
+                reg.style.visibility = "hidden"
+                verify.style.visibility = "hidden"
+            })
+        }
+
+        let promo_images = ["./Images/1.jpg","./Images/2.jpg","./Images/3.jpg","./Images/4.jpg","./Images/5.jpg"]
+        let slide = document.getElementById("slide")
+
+        let images_count = 0;
+
+        setInterval(() => {
+            slide.setAttribute("src",promo_images[images_count])
+            images_count++    
+            if (images_count === 5){
+                images_count = 0
+            }
+        }, 3000);
+
+// ============================================================
+// open close animation all part done
+// below need to wrok on login and re directing to new page and changing all link 
+
+let reg_form = document.getElementById("reg_form")
+let reg_data = JSON.parse(localStorage.getItem("reg_data")) || []
+let regg22 = document.getElementById("reg22")
+let reg22_h1 = document.createElement("h1")
+        reg_form.addEventListener("submit",(e)=>{
+            e.preventDefault()
+            let reg_obj = {
+                email : reg_form.reg_email.value,
+                reg_number : reg_form.reg_number.value,
+                reg_password : reg_form.reg_password.value
+            }
+           
+            reg_data.forEach(element => {
+                if (element.reg_number === reg_form.reg_number.value || element.reg_email === reg_form.reg_email.value ){
+                    regg22.innerHTML = ""
+                    reg22_h1.innerText = "Hey You Are Already registerd with us please login"
+                    regg22.append(reg22_h1)
+
+                setTimeout(() => {
+                reg2.style.visibility = "hidden"
+                reg.style.visibility = "visible"
+                verify.style.visibility = "hidden"
+
+                }, 2500);
+                }else {
+                    reg_data.push(reg_obj);
+                    
+                    regg22.innerHTML = ""
+                    reg22_h1.innerText = "Congrats Your regitration Is Successful"
+                    regg22.append(reg22_h1)
+
+                    setTimeout(() => {
+                        reg2.style.visibility = "hidden"
+                        reg.style.visibility = "visible"
+                        verify.style.visibility = "hidden"
+        
+                        }, 2500);
+                }
+            });
+           
+            localStorage.setItem("reg_data",JSON.stringify(reg_data))
+
+        })
+
+        
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++?
+
+
+        function login_task(login){
+            let verify_password = document.getElementById("verify_password")
+            reg_data.forEach(element => {
+                if (element.reg_number === login.mobile.value){
+                    verify_password.addEventListener("submit",(e)=>{
+                        e.preventDefault()
+                        if (element.reg_password === verify_password.verify_password.input.value){
+                            console.log("yes")
+                        }
+                    })
+                }
+            });
+        }
